2aaa16e85565752413b7a8663a22721b086cc147
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-05 14:12:28 +0100

    json + toast

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Toast } from 'vue-toastification'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Toast, {
+    position: 'top-right',
+    timeout: 2000,
+    closeOnClick: true,
+    pauseOnHover: true,
+})
 app.mount('#app')
--- src/stores/idopont.js
+++ src/stores/idopont.js
@@ -1,11 +1,17 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios';
 
 export const useIdopontStore = defineStore('idopont', () => {
   const idontok = ref({});
   
   const fetchIdopontok = async() =>{
+    const response = await axios.get("localhost:3000/idopontok")
+    idontok.value = response.data
+  }
 
+  const pushIdopont = async() =>{
+    
   }
 
   return { idopontok, fetchIdopontok }
