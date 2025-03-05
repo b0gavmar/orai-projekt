33ccd61d3264dc9adc44c78c7fbe31896cef1041
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-05 14:56:21 +0100

    napot irja ki

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@ onMounted(async () => {
     <div class="container d-flex flex-wrap justify-content-center">
       <div v-for="idopont in idopontStore.idopontok" class="card bg-dark text-light m-2 p-1">
         <div class="card-text">
-          <p> {{ idopont.date }} </p>
+          <p> {{ idopont.day }} </p>
           <p>{{ idopont.hour }}</p>
         </div>
       </div>
