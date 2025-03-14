7a447de6a1ad64d3983f725ff4353b0816ac9b48
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-14 08:23:09 +0100

    nem dob vissza

--- src/stores/idopont.js
+++ src/stores/idopont.js
@@ -1,48 +1,52 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import axios from 'axios';
-import { useToast } from 'vue-toastification';
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
+import { useToast } from "vue-toastification";
 
 const toast = useToast();
 
-export const useIdopontStore = defineStore('idopont', () => {
+export const useIdopontStore = defineStore("idopont", () => {
   const idopontok = ref([]);
-  const foglalniKivantIdopont = ref({})
+  const foglalniKivantIdopont = ref({});
 
   const fetchIdopontok = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/idopontok")
-      idopontok.value = response.data
+      const response = await axios.get("http://localhost:3000/idopontok");
+      idopontok.value = response.data;
+    } catch {
+      toast.error("Nem lehetett lekérni az időpontokat");
     }
-    catch {
-      toast.error("Nem lehetett lekérni az időpontokat")
-    }
-  }
+  };
 
-  const getIdopont = async(id) => {
+  const getIdopont = async (id) => {
     try {
-      await fetchIdopontok()
-      const response = await axios.get(`http://localhost:3000/idopontok/${id}`)
-      return response.data
+      await fetchIdopontok();
+      const response = await axios.get(`http://localhost:3000/idopontok/${id}`);
+      return response.data;
+    } catch {
+      toast.error("Nem lehetett lekérni az időpontokat");
     }
-    catch {
-      toast.error("Nem lehetett lekérni az időpontokat")
-    }
-  }
+  };
 
-  const pushIdopont = async (idopont) => {
+  const postIdopont = async (idopont) => {
     try {
-      await axios.push("http://localhost:3000/idopontok")
-      toast.success("Sikeres foglalás")
+      await axios.post("http://localhost:3000/idopontok", idopont);
+      toast.success("Sikeres foglalás");
+    } catch {
+      toast.error("Nem sikerült leadni a foglalást");
     }
-    catch {
-      toast.error("Nem sikerült leadni a foglalást")
-    }
-  }
+  };
 
-  const setFoglalniKivantIdopont = (idopont) =>{
-    foglalniKivantIdopont.value = idopont.value
-  }
+  const setFoglalniKivantIdopont = (idopont) => {
+    foglalniKivantIdopont.value = idopont;
+  };
 
-  return { idopontok, foglalniKivantIdopont, setFoglalniKivantIdopont, fetchIdopontok, getIdopont, pushIdopont }
-})
+  return {
+    idopontok,
+    foglalniKivantIdopont,
+    setFoglalniKivantIdopont,
+    fetchIdopontok,
+    getIdopont,
+    postIdopont,
+  };
+});
--- src/views/IdopontFoglalasView.vue
+++ src/views/IdopontFoglalasView.vue
@@ -3,10 +3,11 @@ import { ref, onMounted } from "vue";
 import { useIdopontStore } from "@/stores/idopont.js";
 import { useRoute } from "vue-router";
 import { useToast } from "vue-toastification";
+import router from '@/router'
 
-const idopontStore = useIdopontStore()
-const route = useRoute()
-const toast = useToast()
+const idopontStore = useIdopontStore();
+const route = useRoute();
+const toast = useToast();
 
 const currentIdopont = ref({
   id: "0",
@@ -15,19 +16,23 @@ const currentIdopont = ref({
   name: "",
   mobile: "",
   reserved: false,
-})
+});
 
-const reserve = async() =>{
-  if(currentIdopont.name.trim === '' || currentIdopont.mobile.trim === ''){
-    toast.error("Töltsön ki minden mezőt!")
+const reserve = async () => {
+  if (currentIdopont.value.name.trim() == "" || currentIdopont.value.mobile.trim() == "") {
+    toast.error("Töltsön ki minden mezőt!");
   }
-  await idopontStore.pushIdopont(currentIdopont)
-}
+  else{
+    currentIdopont.value.reserved = true;
+    await idopontStore.postIdopont(currentIdopont.value);
+    router.push("/")
+  }
+};
 
 onMounted(async () => {
-  await idopontStore.fetchIdopontok();
-  const foundIdopont = await idopontStore.getIdopont(route.params.idopontid);
-  currentIdopont.value = { ...foundIdopont };
+  currentIdopont.value = await idopontStore.foglalniKivantIdopont;
+  currentIdopont.name = "";
+  currentIdopont.mobile = "";
 });
 </script>
 
@@ -60,10 +65,12 @@ onMounted(async () => {
             class="form-control"
             id="telsz"
             name="telsz"
-            placeholder="Adja meg az email címét"
+            placeholder="Adja meg a telefonszámát"
           />
         </div>
-        <button type="submit" class="btn btn-primary mt-4" @click="reserve">Foglalás</button>
+        <button type="submit" class="btn btn-primary mt-4" @click="reserve()" id="foglalasgomb">
+          Foglalás
+        </button>
       </div>
       <div v-else>
         <h2>Az időpont már le van foglalva!</h2>
